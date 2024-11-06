0a4ae27cb26b1ce2d70fe50ecc2c672a24b47d53
MustBeViable <elias1rinne@gmail.com>
2024-11-06 13:55:26 +0200

    8th done

--- Module3/module3-starters-main/module3-starters-main/t8/8.js
+++ Module3/module3-starters-main/module3-starters-main/t8/8.js
@@ -5,15 +5,14 @@ const button = document.querySelector('#start');
 button.addEventListener('click', function() {
   let num11 = document.querySelector('#num1');
   let num21 = document.querySelector('#num2');
-  let num1 = parseInt(num11.value);
-  let num2 = parseInt(num21.value);
+  let num1 = parseFloat(num11.value);
+  let num2 = parseFloat(num21.value);
   let option = document.getElementById('operation');
   let calculation = option.value;
   let result = document.querySelector('#result');
   let results;
   if (calculation === 'add') {
-    results = (num1 + num2);
-    result.textContent = results;
+    result.textContent = (num1 + num2);
   } else if (calculation === 'sub') {
     results = (num1 - num2);
     result.textContent = results;
