f5f777811e3d4a1a46dd6ae0a494c423df899e57
MustBeViable <elias1rinne@gmail.com>
2024-11-06 13:45:32 +0200

    8th done

---
+++ Module3/module3-starters-main/module3-starters-main/t8/8.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const button = document.querySelector('#start');
+
+button.addEventListener('click', function() {
+  let num11 = document.querySelector('#num1');
+  let num21 = document.querySelector('#num2');
+  let num1 = parseInt(num11.value);
+  let num2 = parseInt(num21.value);
+  let option = document.getElementById('operation');
+  let calculation = option.value;
+  let result = document.querySelector('#result');
+  let results;
+  if (calculation === 'add') {
+    results = (num1 + num2);
+    result.textContent = results;
+  } else if (calculation === 'sub') {
+    results = (num1 - num2);
+    result.textContent = results;
+  } else if (calculation === 'multi') {
+    result.textContent = (num1 * num2);
+  } else {
+    result.textContent = (num1 / num2);
+  }
+});
